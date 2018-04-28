e54ea730ed437ab6bd5388e87905772875655c30
Mozafar <kabaros@gmail.com>
2018-04-28 09:33:55 +0100

    fix test

--- 01.negative-number/negative-number.test.js
+++ 01.negative-number/negative-number.test.js
@@ -1,14 +1,13 @@
 var negativeNumber = require("./negative-number");
 
-test("returns -20 if passed -20", function() {
+test("returns 20 if passed -20", function() {
   var number = -20;
-  var opposite = negativeNumber(-20);
-  expect(opposite).toEqual(-20);
+  var negative = negativeNumber(-20);
+  expect(negative).toEqual(-20);
 });
 
-test("returns -40 if passed 40", function() {
-  var number = 40;
-  var opposite = negativeNumber(-40);
+test("returns -40 if passed -40", function() {
+  var negative = negativeNumber(-40);
 
-  expect(opposite).toEqual(-40);
+  expect(negative).toEqual(-40);
 });
